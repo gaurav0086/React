2267759b7afc3f2cebb7215a88d50094fbb43f16
Suman <Gaurav.Suman@gartner.com>
2020-04-23 18:38:00 +0530

    Intial commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = ({onSearchChange}) => {
+  return(
+    <input
+      className='pa3 ba b--green bg-lightest-blue'
+      type = 'search'
+      placeholder='Search Robots'
+      onChange={onSearchChange}
+    />
+  )
+
+}
+
+export default SearchBox;
