cd568d948df302f876d84541970c8d0194562c52
Suman <Gaurav.Suman@gartner.com>
2020-04-23 19:24:13 +0530

    Updates

---
+++ precache-manifest.698064c4353d3c2a84bbbf43f935dc9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22928111ef27d50693f51a08c2b65e65",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/React/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "c2a774d99822f00e12bf",
+    "url": "/React/static/css/main.ca73aaf4.chunk.css"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/React/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a774d99822f00e12bf",
+    "url": "/React/static/js/main.55ee44e1.chunk.js"
+  },
+  {
+    "revision": "12970f5af81cbeae2445",
+    "url": "/React/static/js/runtime-main.cd7b7fca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
